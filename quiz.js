b179769f45f6dbce94b7852f41b2b1eac9ff2ec9
Prateek Sahay <ptkinvent@gmail.com>
2020-12-11 23:21:01 -0800

    Switched all questions from h5 to h6

--- quiz.js
+++ quiz.js
@@ -22,7 +22,7 @@ class ExplanationQuestion {
 
     render(wrapper) {
         // Question text
-        let question = document.createElement('h5');
+        let question = document.createElement('h6');
         question.setAttribute('class', 'mt-4');
         question.innerHTML = this.questionText;
         wrapper.appendChild(question);
@@ -40,7 +40,7 @@ class TextQuestion {
         let answerText = this.answerText;
 
         // Question text
-        let question = document.createElement('h5');
+        let question = document.createElement('h6');
         question.setAttribute('class', 'mt-4');
         question.innerHTML = questionText;
         wrapper.appendChild(question);
@@ -83,7 +83,7 @@ class NumericalQuestion {
         let questionId = this.questionText;
         let answerFunc = this.answerFunc;
 
-        let question = document.createElement('h5');
+        let question = document.createElement('h6');
         question.setAttribute('class', 'mt-4');
         question.innerHTML = this.questionText;
         wrapper.appendChild(question);
@@ -184,7 +184,7 @@ class MultipleChoiceQuestion {
     render(wrapper) {
         let questionText = this.questionText;
 
-        var question = document.createElement('h5');
+        var question = document.createElement('h6');
         question.setAttribute('class', 'mt-4');
         question.innerHTML = questionText;
         wrapper.appendChild(question);
